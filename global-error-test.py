e5e38fa53066b1c4a975551387d09627dfe00b56
Marc Owen <marcowen20@gmail.com>
2018-07-23 06:14:26 +0800

    The bug is fixed
    Y1 and Y2 was fixed to y0 and y1

--- global-error-test.py
+++ global-error-test.py
@@ -1,5 +1,4 @@
 import methods
-import standalone_calculation_expanded as sce
 import standalone_calculation as sc
 
 # Case 1, 0.2
@@ -19,17 +18,10 @@ def GetCase1_0_2():
     sc_differences = [(i, value) for i, value in enumerate([abs(sc_exactResults[i][1] - sc_approximateResults[i][1]) for i in range(step + 1)])]
     sc_diffValue = [value[1] for value in sc_differences]
 
-    # Expanded
-    sce_exactResults = [(i, value) for i,value in enumerate(sce.GetExactValues(step, k, m, y_0, y_0_prime, h))]
-    sce_approximateResults = [(i, value) for i,value in enumerate(sce.GetApproximateValues(step, k, m, y_0, y_0_prime, h, ref))]
-    sce_differences = [(i, value) for i, value in enumerate([abs(sce_exactResults[i][1] - sce_approximateResults[i][1]) for i in range(step + 1)])]
-    sce_diffValue = [value[1] for value in sce_differences]
-
     print("Case 1, 0.2")
     for i in range(step + 1):
         print("{} : {} : {}".format(sc_exactResults[i], sc_approximateResults[i], sc_differences[i]))
 
     print("Global error (Not Expanded): {}".format(max(sc_diffValue)))
-    print("Global error (Expanded): {}".format(max(sce_diffValue)))
 
 GetCase1_0_2()
--- standalone_calculation.py
+++ standalone_calculation.py
@@ -8,7 +8,6 @@ def GetExactValues(step, springConstant, mass, initialDisplacement, velocity, ti
 
     h_squared  = h**2
     omega_0 = np.sqrt(k/m)
-    print(omega_0)
     gamma_1 = y_0
     gamma_2 = y_0_prime/omega_0
 
@@ -38,15 +37,15 @@ def GetApproximateValues(step, springConstant, mass, initialDisplacement, veloci
     y_1 = GetExactValues(step, k, m, y_0, y_0_prime, h)[1]
     results.append(y_1)
 
-    Y1 = results[-2]
-    Y2 = results[-1]
-
     if step == 0:
-        return Y1
+        return results[0]
     elif step == 1:
-        return Y2
+        return results[1]
 
     for x in range(2, step + 1):
+        Y1 = results[-2]
+        Y2 = results[-1]
+        
         sigma_i_3 = ref.GetValue("a31")*Y1*(-k/m) + ref.GetValue("a32")*Y2*(-k/m)
         Y3_front = results[-1]*(1 + ref.GetValue("c3")) - results[-2]*ref.GetValue("c3")
         Y3 = Y3_front + (h**2)*(sigma_i_3)
@@ -65,7 +64,5 @@ def GetApproximateValues(step, springConstant, mass, initialDisplacement, veloci
 
         sigma_f_Y_i = ref.GetValue("b3")*Y3*(-k/m) + ref.GetValue("b4")*Y4*(-k/m) + ref.GetValue("b5")*Y5*(-k/m)
         Y_n_plus_1 = 2*results[-1] - results[-2] + h**2 * (ref.GetValue("b1")*results[-2]*(-k/m) + ref.GetValue("b2")*results[-1]*(-k/m) + sigma_f_Y_i)
-
-        # TODO: Expand final equation to Y1 and Y2
         results.append(Y_n_plus_1)
     return(results)
